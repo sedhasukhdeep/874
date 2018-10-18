478f94bd30f3a78de6f5bf729564d2c5da58003e
Sukhdeep Singh <sedhasukhdeep@gmail.com>
2018-10-18 16:34:18 +1100

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -2,22 +2,21 @@ import json
 import sys
 import nltk
 import re
-
-#(\/\*\s+(\d+)\s+\*\/) <- remove /* 1*/
-#(\"([^\"]+)\"\s+\:\s+(\w+)\(\"([^\"]+)\"\)\,) <- remove object id
-#(\"([^\"]+)\"\s+\:\s+(\w+)\((\d+)\)\,)<- remove id
-
-
+#Loading the clean Json file
 with open('10000 tweets_old.json', 'r', encoding="utf8") as f:
-    data = json.load(f)
+ data = json.load(f)
+#Iterating over the keys of the json dicionary
 
-    
 for i in data:
-    
-    text =i['text']
-    text= re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text)
-    tokens = nltk.word_tokenize(text)
-    
-    for word in tokens:
-        
-        print("{0}\t{1}".format(word,1))
+
+ text =i['text'] #This grabs the text key from the tweet
+#Here we are passing it to tweet cleaning regex to remove things like special characters
+and URL
+ text= re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text)
+#Using the NLTK tokenize function to generate tokens of the sentence
+
+ tokens = nltk.word_tokenize(text)
+#Outputting the Key Value pairs to STDIN
+for word in tokens:
+
+ print("{0}\t{1}".format(word,1)) 
